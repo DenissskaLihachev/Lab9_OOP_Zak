6d9b7358b78bb80b00bdb8c47b0ae6cb457669b7
lihac <lihac@DESKTOP-C1L3RKG>
2022-12-18 16:36:47 +0800

    Добавьте файлы проекта.

---
+++ Lab9_OOP/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace Lab9_OOP
+{
+	class Program
+	{
+		delegate bool CheckNum(int num);
+		static void Main(string[] args)
+		{
+			while(true)
+			{
+				Console.Write("1) Задание 1\n2) Задание 2\nВвод: ");
+				int choice = Convert.ToInt32(Console.ReadLine());
+				Console.Clear();
+				switch(choice)
+				{
+					case 1:
+						{
+							Task1();
+							break;
+						}
+					case 2:
+						{
+							Task2();
+							break;
+						}
+				}
+			}
+			
+		}
+
+		static void Task1()
+		{
+			CheckNum checkNum;
+			bool GetEven(int num) => num % 2 == 0 ? true : false;
+			bool GetPositive(int num) => num > 0 ? true : false;
+			int Counting(int[] masNum, CheckNum checkNum, out int count)
+			{
+				count = 0;
+				for (int i = 0; i < masNum.Length; i++)
+				{
+					if (checkNum(masNum[i]))
+					{
+						count++;
+					}
+				}
+				return count;
+			}
+			CheckNum GetPrevious()
+			{
+				Console.Write("Числа меньше заданного (введите число): ");
+				int numLess = Convert.ToInt32(Console.ReadLine());
+				bool check;
+				bool Less(int num)
+				{
+					check = false;
+					check = num < numLess ? true : false;
+					return check;
+				}
+				return Less;
+			}
+
+			checkNum = GetEven;
+			int[] mas = new int[10];
+			Random rand = new Random();
+
+			Console.Write("Массив [");
+			for (int i = 0; i < mas.Length; i++)
+			{
+				mas[i] = rand.Next(-10, 10);
+				Console.Write($" {mas[i]} ");
+			}
+			Console.WriteLine("]");
+
+
+			Console.WriteLine("Чётных чисел: " + Counting(mas, checkNum, out int count));
+			checkNum = GetPositive;
+			Console.WriteLine($"Положительные числа: " + Counting(mas, checkNum, out count));
+			checkNum = GetPrevious();
+			Console.WriteLine("Числа меньше введенного: " + Counting(mas, checkNum, out count));
+			Console.ReadKey(); Console.Clear();
+		}
+		static void Task2()
+		{
+			void PrintEvent(string message) => Console.WriteLine(message);
+			int size;
+
+			Console.Write("Введите размер массива: "); size = Convert.ToInt32(Console.ReadLine());
+			Console.Clear();
+
+			UniqIntMas obj = new UniqIntMas(size);
+
+			Console.Write("Хотите ли вы получать уведомления о действиях?\n1) Да\n2) Нет\nВвод: "); int eve = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+			switch(eve)
+			{
+				case 1:
+					{
+                        obj.Notify += PrintEvent;
+                        break;
+					}
+				case 2:
+					{
+
+						break;
+					}
+			}
+
+			while (true)
+			{
+				Console.Clear();
+
+				Console.Write("1) Добавить элемент\n2) Удалить элемент\n3) Вывести массив\n0) Выйти\nВвод: ");
+				int choice = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+
+                switch (choice)
+				{
+					case 1:
+						{							
+							Console.Write("Введите число: ");
+							int num = Convert.ToInt32(Console.ReadLine());
+							obj.Add(num);
+							Console.ReadKey();
+							break;
+						}
+					case 2:
+						{
+							Console.Write("Введите число: ");
+							int num = Convert.ToInt32(Console.ReadLine());
+							obj.Remove(num);
+							Console.ReadKey();
+							break;
+						}
+					case 3:
+						{
+							Console.Write("Массив: [ ");
+							obj.Print();
+                            Console.Write("]");
+                            Console.ReadKey();
+							break;
+						}
+				}
+				if (choice == 0)
+					break;
+			}
+            Console.Clear();
+        }
+	}
+}
---
+++ Lab9_OOP/UniqIntMas.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_OOP
+{
+    internal class UniqIntMas
+    {
+
+        public int?[] arr;
+        public int size;
+        public delegate void HandlerEvent(string mes);
+        public event HandlerEvent? Notify;
+
+        public UniqIntMas(int size)
+        {
+            this.size = size;
+            arr = new int?[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = null;
+            }
+        }
+
+        public void Add(int num)
+        {
+            int i;
+            for (i = 0; arr[i].HasValue && i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    Console.WriteLine($"Такой элемент уже существует");
+                    return;
+                }
+            }
+            if (i == arr.Length - 1) Console.WriteLine($"Массив заполнен");
+            else
+            {
+                arr[i] = num;
+                if (Notify != null)
+                    Notify.Invoke($"Добавлен новый элемент [{num}]");
+            }
+        }
+        public void Remove(int num)
+        {
+            int i;
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    arr[i] = null;
+                    if (Notify != null)
+                        Notify.Invoke($"Удалён элемент [{num}]");
+                    return;
+                }
+            }
+            if (i == arr.Length)
+            {
+                Console.WriteLine($"Такого элемента не существует");
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].HasValue)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+        }
+    }
+}
